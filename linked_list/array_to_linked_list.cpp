ffab6f158b84d525fc9b8b6af11965e1e4d9c29b
Sanket <sanketgudade006@gmail.com>
2026-09-01 19:44:29 +0530

    CSES-stared

--- linked_list/array_to_linked_list.cpp
+++ linked_list/array_to_linked_list.cpp
@@ -1,10 +1,12 @@
-#include<stdio.h>
-#include<conio.h>
-
+#include<iostream>
+#include <iterator>
+using namespace std;
 // creating node structure
 class Node{
+    public:
     int data;                 
     Node* next;
+    
     Node(int value){
         data=value;
         next=nullptr;
@@ -14,7 +16,30 @@ class Node{
 
 int main(){
 
+  int arr[4]={1,2,3,4};
+  int n=sizeof(arr)/sizeof(arr[0]);
+
+  Node* head=new Node(arr[0]);
+  Node*temp =head;
+  //converting the ARRAY to the LINKLIST
+
+  for(int i=1;i<n;i++){
+    temp->next=new Node(arr[i]);
+    temp=temp->next;
+  }
   
+  //Printing the array
+  //bring the temp to head again which is present at the end now
+
+  temp=head;
+
+  while(temp!=nullptr){
+    cout<<temp->data<<"->";
+    temp=temp->next;
+  }
+
+  cout<<"NULL"<<endl;
+
 
     return 0;
 }
